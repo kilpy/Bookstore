7c8f92ea02c74b38683f3400c6b96ff48291a2ce
kilpy <82245588+kilpy@users.noreply.github.com>
2022-03-15 18:45:02 -0400

    Add files via upload

--- updateshipping.php
+++ updateshipping.php
@@ -1,5 +1,12 @@
 <?php
 require_once "config.php";
+
+$fprice = $priceerr = "";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $fprice = htmlspecialchars($_POST['price']);
+    if(empty($fprice)) $priceerr = "&nbsp;&nbsp;Please enter a valid price";
+    else $price = floatval($fprice);
+}
 ?>
 
 <html>
@@ -14,14 +21,15 @@ require_once "config.php";
             $result = mysqli_query($dbConnect, $query);
             while ($row = mysqli_fetch_assoc($result)) {
                 $selected = $result == $row['method'] ? "selected" : "";
-                echo "<option {$selected} value = \"{$row['method']}\">{$row['price']} </option>\n";
+                echo "<option {$selected} value = \"{$row['method']}\">{$row['method']} {$row['price']} </option>\n";
             }
             mysqli_free_result($result);
             ?>
-
-<?php
-   
-   echo 'Manage Shipping Methods';
-   
-
-?>
+            <br><br>
+        Update Price:<select name="Price"> <input type="number" name="price" size="8" min="0.01" max="10000.00" step="0.01" value=<?php echo $fprice ?>><?php echo $priceerr?><br><br>
+            <?php
+            $query = "UPDATE ShippingMethods SET price = $fprice WHERE method = $selected";
+            $result = mysqli_query($dbConnect, $query);
+            mysqli_free_result($result);
+            ?>
+            
